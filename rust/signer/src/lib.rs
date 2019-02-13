8cdb604bc27140651c93e0ebd302eadd316ca265
Maciej Hirsz <maciej.hirsz@gmail.com>
2019-02-13 18:54:57 +0100

    feat(rust): Porting to ethsign

--- rust/signer/src/lib.rs
+++ rust/signer/src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright 2015-2017 Parity Technologies (UK) Ltd.
+// Copyright 2015-2019 Parity Technologies (UK) Ltd.
 // This file is part of Parity.
 
 // Parity is free software: you can redistribute it and/or modify
@@ -15,6 +15,7 @@
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
 mod string;
+mod brain;
 
 use parity_wordlist as wordlist;
 use rustc_serialize::hex::{ToHex, FromHex};
